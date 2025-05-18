1edd2297076d4ea63b3af866ce97d00cb129c572
SuhanKS <promsedear@gmail.com>
2025-05-18 13:25:47 +0600

    Hero section has been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./component/header";
+import Hero from "./component/hero";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <Hero></Hero>
     </>
   );
 }
