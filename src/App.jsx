012e2084a1abc23e9cb312f9e6684e646417a542
SuhanKS <promsedear@gmail.com>
2025-05-20 14:28:11 +0600

    Trial section has been done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Pricing from "./component/Pricing";
 import Service1 from "./component/Service1";
 import Service2 from "./component/Service2";
 import Testimonial from "./component/Testimonial";
+import Trial from "./component/Trial";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Testimonial></Testimonial>
       <Pricing></Pricing>
       <Faq></Faq>
+      <Trial></Trial>
     </>
   );
 }
